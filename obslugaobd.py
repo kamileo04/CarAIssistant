aa4e917d216f315af877ff36f6011ed5568466ca
Maykello <kontgames122@gmail.com>
2025-01-06 20:51:09 +0100

    drobna edycja kodu oraz sprawdzenie dzialania na ecu silnika i interfejsie stn2120

---
+++ obslugaobd.py
@@ -0,0 +1,31 @@
+﻿# >python -m pip install obd
+# wersja python 3.12
+# nudził żeby zapisać plik z kodowaniem UTF-8
+
+import obd
+
+connection = obd.OBD()
+#obd.OBD(port='COM3')
+
+if connection.is_connected():
+    print("Połączono z adapterem OBD-II")
+else:
+    print("Nie udalo sie połączyć z adapterem OBD-II")
+    exit()
+
+def check(query, description):
+    response = connection.query(query)
+    if response.is_null():
+        print(f"Błąd odczytu {description}")
+    else:
+        print(f"{description}: {response.value}")
+
+check(obd.commands.RPM , "Obroty silnika")
+check(obd.commands.INTAKE_PRESSURE, "Cisnienie turbo")
+check(obd.commands.MAF, "Przepływomierz powietrza")
+check(obd.commands.INTAKE_TEMP, "Temperatura oleju")
+check(obd.commands.SPEED, "Predkosc pojazdu")
+check(obd.commands.FUEL_LEVEL, "Poziom paliwa")
+check(obd.commands.THROTTLE_POS, "Pozycja przepustnicy (pedał gazu)")
+
+connection.close()
