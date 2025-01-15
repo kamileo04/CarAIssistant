a74b78b28394b67f049b86810901bdf6bf95ec36
Kamil Kowalski <86122755+kamileo04@users.noreply.github.com>
2025-01-15 19:28:58 +0100

    test jednostkowy dodany

---
+++ testnawigacji.py
@@ -0,0 +1,24 @@
+﻿import unittest
+from NavigationSystem import NavigationSystem
+
+class TestNavigationSystem(unittest.TestCase):
+
+    def setUp(self):
+        self.navigation_system = NavigationSystem("Warszawa", "Kraków")
+
+    def test_calculate_route(self):
+        self.assertEqual(self.navigation_system.calculateRoute("Gdańsk"), "Trasa obliczona")
+
+    def test_provide_eta(self):
+        self.assertEqual(self.navigation_system.provideETA(), 60)
+
+    def test_set_current_location(self):
+        self.navigation_system.setCurrentLocation("Poznań")
+        self.assertEqual(self.navigation_system.currentLocation, "Poznań")
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
